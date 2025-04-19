507ed89feefc6fd69e33c47dc0880edd62e6c731
patte <p@tte.io>
2025-04-19 21:42:49 +0200

    feat: export script from dom.js

--- svelte-themes/src/lib/index.ts
+++ svelte-themes/src/lib/index.ts
@@ -2,3 +2,4 @@ export { getTheme, hasTheme } from './context.js';
 export { default as ThemeProvider } from './theme-provider.svelte';
 export type { Config } from './config.js';
 export { HydrationWatcher } from './utils.svelte.js';
+export { script as themeScript } from './dom.js';
